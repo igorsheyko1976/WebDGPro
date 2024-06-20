4c7889b1fb5557f179921861eff568afcd2d19f3
igorsheyko1976 <sheyko-iv@mail.ru>
2024-06-20 21:59:12 +0300

    Initial commit

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('data', views.data),
-    path('test', views.test)
-]
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django<h1>")
 
-def data(request):
-    return HttpResponse("<h1>Это вторая страница моего проекта на Django<h1>")
+def home(request):
+    return render(request, 'main/home.html')
 
-def test(request):
-    return HttpResponse("<h1>Это третья страница моего проекта на Django<h1>")
+def about(request):
+    return render(request, 'main/about.html')
 
+def services(request):
+    return render(request, 'main/services.html')
 
-
-
+def contact(request):
+    return render(request, 'main/contact.html')
